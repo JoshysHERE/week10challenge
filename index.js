6b8985e524855abe9fd144c27209d4225f40a940
Joshua <Joshua.soto@live.com>
2023-10-30 17:14:18 -0500

    added questions

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-const filesystem = require('./node')
+const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require('inquirer');
+const {Circle, Square, Triangle} = require('./lib/shapes');
+
+class Svg{
+    constructor(){
+  this.textElement = ''
+  this.shapeElement = ''
+    }
+
+    render(){
+       
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElements(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
+    //Array for questions
+
+    const questions = [
+        {
+              type: 'input',
+              name: 'text',
+              message: 'TEXT: Enter up to {3} Characters ?',
+            },
+            {
+              type: 'input',
+              name: 'text-color',
+              message: 'Text Color: Enter a color keyword! (or hex code):',
+              
+            },
+            {
+              type: 'input',
+              name: 'shape',
+              message: 'Shape Color: Enter a color keyword! (or hex code):',
+            },
+            {
+                type: "list",
+                name: "pixel-image",
+                message: "Choose which Pixel Image you would like?",
+                choices: ["Circle", "Square", "Triangle"],
+            },
+        ];
+        
