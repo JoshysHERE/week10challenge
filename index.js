b85fbdc9149815b584d9613e0a2d1ef2f41b928b
Joshua <Joshua.soto@live.com>
2023-10-30 17:34:30 -0500

    added shapes js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
-const inquirer = require('inquirer');
-const {Circle, Square, Triangle} = require('./lib/shapes');
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
 class Svg{
     constructor(){
-  this.textElement = ''
-  this.shapeElement = ''
+        this.textElement = ''
+        this.shapeElement = ''
     }
-
     render(){
-       
+
         return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     }
-    setTextElements(text,color){
+    setTextElement(text,color){
         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
     }
     setShapeElement(shape){
         this.shapeElement = shape.render()
+
     }
+    
 }
+
     //Array for questions
 
     const questions = [
@@ -45,4 +47,16 @@ class Svg{
                 choices: ["Circle", "Square", "Triangle"],
             },
         ];
+
+        function writeToFile(fileName, data) {
+            console.log("Writing [" + data + "] to file [" + fileName + "]")
+            filesystem.writeFile(fileName, data, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("Congratulations, you have Generated a logo.svg!");
+            });
+        }
+
+        
         
